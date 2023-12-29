d9143b1f549d567256721fd2fb09a5360a4ca99d
hendisantika <hendisantika@yahoo.co.id>
2023-12-29 09:26:56 +0700

    Create starter project 🫸🌀✏️📗 :octocat:

---
+++ src/test/java/com/hendisantika/redisexample/SpringWebfluxRedisExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.redisexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxRedisExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
