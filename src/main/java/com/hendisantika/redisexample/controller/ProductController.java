9d5265369af1b9dd15928b2188b75bd4949af92f
hendisantika <hendisantika@yahoo.co.id>
2023-12-29 09:39:28 +0700

    Add getProduct in ProductController 🫸🌀✏️📗 :octocat:

--- src/main/java/com/hendisantika/redisexample/controller/ProductController.java
+++ src/main/java/com/hendisantika/redisexample/controller/ProductController.java
@@ -11,10 +11,16 @@ package com.hendisantika.redisexample.controller;
  * To change this template use File | Settings | File Templates.
  */
 
+import com.hendisantika.redisexample.model.ProductResponse;
 import com.hendisantika.redisexample.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @Slf4j
 @RestController
@@ -22,5 +28,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     private final ProductService productService;
+
+    /**
+     * Gets the product data for a specific product.
+     *
+     * @param id product identifier
+     * @return a {@link ProductResponse} if found
+     */
+    @GetMapping(value = "/products/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<ResponseEntity<ProductResponse>> getProduct(@PathVariable("id") String id) {
+        return productService.getProduct(id)
+                .map(product -> ResponseEntity.ok(ProductResponse.from(product)))
+                .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
+    }
 
 }
