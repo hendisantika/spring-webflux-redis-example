1c58b89f2513aa1f7d255a96313eafd99ba0753f
hendisantika <hendisantika@yahoo.co.id>
2023-12-29 09:34:44 +0700

    Add loadData in ProductLoader 🫸🌀✏️📗 :octocat:

--- src/main/java/com/hendisantika/redisexample/data/ProductLoader.java
+++ src/main/java/com/hendisantika/redisexample/data/ProductLoader.java
@@ -1,11 +1,13 @@
 package com.hendisantika.redisexample.data;
 
 import com.hendisantika.redisexample.model.Product;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisOperations;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,4 +26,17 @@ public class ProductLoader {
     private final ReactiveRedisConnectionFactory connFactory;
     private final ReactiveRedisOperations<String, Product> productOps;
 
+    @PostConstruct
+    public void loadData() {
+        connFactory.getReactiveConnection()
+                .serverCommands()
+                .flushAll()
+                .thenMany(Flux.range(1, 10)
+                        .map(this::generateProduct)
+                        .flatMap(product -> productOps.opsForValue().set(product.getId(), product))
+                )
+                .doOnSubscribe(subscription -> log.info("Loading test data..."))
+                .doOnComplete(() -> log.info("Test data load complete!"))
+                .subscribe();
+    }
 }
